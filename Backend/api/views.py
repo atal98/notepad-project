2426117e66c06ce1b482b17c3b89632b4b879fc7
atal98 <80337520+atal98@users.noreply.github.com>
2024-03-12 16:55:18 +0530

    backend intial api created

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+from .serializers import NoteSerializer
+from .models import Note
 
-# Create your views here.
+class Routes(APIView):
+    def get(self, request):
+        response = [
+            {
+                'Endpoint': '/notes/',
+                'method': 'GET',
+                'body': None,
+                'description': 'Returns an array of notes'
+            },
+            {
+                'Endpoint': '/notes/id',
+                'method': 'GET',
+                'body': None,
+                'description': 'Returns a single note object'
+            },
+            {
+                'Endpoint': '/notes/create/',
+                'method': 'POST',
+                'body': {'body': ""},
+                'description': 'Creates new note with data sent in post request'
+            },
+            {
+                'Endpoint': '/notes/id/update/',
+                'method': 'PUT',
+                'body': {'body': ""},
+                'description': 'Creates an existing note with data sent in post request'
+            },
+            {
+                'Endpoint': '/notes/id/delete/',
+                'method': 'DELETE',
+                'body': None,
+                'description': 'Deletes and exiting note'
+            }
+        ]
+        
+        return Response(response)
+
+
+class NotesAPI(APIView):
+    
+    def get(self,request):
+        note_qs = Note.objects.all()
+
+        serializer = NoteSerializer(note_qs, many=True)
+        return Response(serializer.data)
+    
+class NoteAPI(APIView):
+    
+    def get(self,request,pk):
+        note_qs = Note.objects.get(id=pk)
+        serializer = NoteSerializer(note_qs, many=False)
+        return Response(serializer.data)
