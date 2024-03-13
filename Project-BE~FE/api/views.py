9cb8efe5c86d19c0a3bb34dace4b3ef4b4600cd4
atal98 <80337520+atal98@users.noreply.github.com>
2024-03-13 20:28:20 +0530

    Backend and Frontend project integrated done and complete project done

--- Project-BE~FE/api/views.py
+++ Project-BE~FE/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import APIView
 from .serializers import NoteSerializer
 from .models import Note
+from rest_framework import status
 
 class Routes(APIView):
     def get(self, request):
@@ -45,9 +46,15 @@ class Routes(APIView):
 class NotesAPI(APIView):
     
     def get(self,request):
-        note_qs = Note.objects.all()
+        note_qs = Note.objects.all().order_by('-updated')
 
         serializer = NoteSerializer(note_qs, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        data = request.data
+        note_qs = Note.objects.create(body = data['body'])
+        serializer = NoteSerializer(note_qs, many=False)
         return Response(serializer.data)
     
 class NoteAPI(APIView):
@@ -56,3 +63,16 @@ class NoteAPI(APIView):
         note_qs = Note.objects.get(id=pk)
         serializer = NoteSerializer(note_qs, many=False)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        note_qs = Note.objects.get(id=pk)
+        serializer = NoteSerializer(note_qs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        note_qs = Note.objects.get(id=pk)
+        note_qs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- Project-BE~FE/mynotes/settings.py
+++ Project-BE~FE/mynotes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mynotes.urls'
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,7 +133,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
